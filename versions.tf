46d7596afbe729428e4c1df7cee04591bb01f892
Samarth Sharma <samarthsharma@Samarths-MacBook-Air-2.local>
2026-07-07 22:55:24 +0530

    versions

--- versions.tf
+++ versions.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    key          = "vpc/terraform.tfstate"
+    key          = "Project/terraform.tfstate"
     encrypt      = true
     use_lockfile = true
   }
