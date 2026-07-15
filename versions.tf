a4a7c345c13bbbcdc95be019e62b995f99252a62
Scaler-Sam <samarth.sharma@scaler.com>
2026-07-15 17:50:12 +0530

    All the files (#3)
    Co-authored-by: Samarth Sharma <samarthsharma@Samarths-MacBook-Air-2.local>

--- versions.tf
+++ versions.tf
@@ -1,10 +1,36 @@
+# versions.tf
+
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.27"
+      version = "~> 6.20"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
     }
   }
 
@@ -19,5 +45,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
